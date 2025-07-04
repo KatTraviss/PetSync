3a97b5a7950a9564bc338dbd83ea0545cf91b759
KatTraviss <163689746+KatTraviss@users.noreply.github.com>
2025-07-03 20:00:51 -0400

    Update User.java
    updated for database

--- src/main/java/model/User.java
+++ src/main/java/model/User.java
@@ -1,18 +1,31 @@
-package model;
+package main.java.model;
 
-import roles.UserRole;
+import java.util.UUID;
+
+import main.java.roles.UserRole;
 
 public class User {
     private String fullName;
     private String email;
     private String password;
-    private UserRole role;
+    private UserRole role; 
+    private UUID userID;
+    private String roleName;
+    
+    public User() {
+    	
+    }
 
-    public User(String fullName, String email, String password, UserRole role) {
-        this.fullName = fullName;
+    public User(UUID userID, String fullName, String email, String password, UserRole role) {
+        this.setUserID(userID);
+    	this.fullName = fullName;
         this.email = email;
         this.password = password;
         this.role = role;
+    }
+    
+    public void roleNameTransfer(String roleName) {
+    	//implement Factory Pattern to set role.
     }
 
     // Show the dashboard based on the user's role
@@ -21,14 +34,22 @@ public class User {
         role.showDashboard();
     }
 
-    // Getters (you can add setters if needed)
+    // Getters & Setters
     public String getFullName() {
         return fullName;
     }
+    
+	public void setFullName(String fullName) {
+		this.fullName = fullName;
+	}
 
     public String getEmail() {
         return email;
     }
+    
+	public void setEmail(String email) {
+		this.email = email;
+	}
 
     public boolean checkPassword(String input) {
         return password.equals(input);
@@ -41,4 +62,29 @@ public class User {
     public UserRole getRole() {
         return role;
     }
+    
+	public void setRole(UserRole role) {
+		this.role = role;
+	}
+	
+	public void setRoleName(String roleName) {
+		this.roleName = roleName;
+	}
+
+	public UUID getUserID() {
+		return userID;
+	}
+
+	public void setUserID(UUID userID) {
+		this.userID = userID;
+	}
+	
+	public void setPassword(String password) {
+		this.password = password;
+	}
+    
+	public String getPassword() {
+		return password;
+	}
+    
 }
